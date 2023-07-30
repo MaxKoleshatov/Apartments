c1724f9d7e612ff32a22040059d9f67b8974fc1c
Max <maksimkoleshatov1993@gmail.com>
2023-07-30 19:45:37 +0500

    User can create announcement

--- app/controllers/daily_apartments_controller.rb
+++ app/controllers/daily_apartments_controller.rb
@@ -6,4 +6,24 @@ class DailyApartmentsController < ApplicationController
   def show
     @daily_apartment = DailyApartment.find(params[:id])
   end
+
+  def new
+    @daily_apartment = DailyApartment.new
+  end
+
+  def create
+    @daily_apartment = current_user.daily_apartments.new(daily_apartment_params)
+
+    if @daily_apartment.save
+      redirect_to @daily_apartment, notice: 'You have created daily announcement'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def daily_apartment_params
+    params.require(:daily_apartment).permit(:square_apartment, :price_daily_apartment, :description_daily)
+  end
 end
--- app/controllers/rent_apartments_controller.rb
+++ app/controllers/rent_apartments_controller.rb
@@ -6,5 +6,26 @@ class RentApartmentsController < ApplicationController
   def show
     @rent_apartment = RentApartment.find(params[:id])
   end
+
+  def new
+    @rent_apartment = RentApartment.new
+  end
+
+  def create
+    @rent_apartment = current_user.rent_apartments.new(rent_apartment_params)
+
+    if @rent_apartment.save
+      redirect_to @rent_apartment, notice: 'You have created rent announcement'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def rent_apartment_params
+    params.require(:rent_apartment).permit(:square_apartment, :price_mount_apartment, :description_rent)
+  end
 end
+  
     
