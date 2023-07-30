a59ae78322dbe1d3d26692b3963d78003b4a3542
Max <maksimkoleshatov1993@gmail.com>
2023-07-30 18:33:07 +0500

    Add Devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   sequence :name do |n|
-    "user#{n}test"
+    "user#{n}nametest"
+  end
+  sequence :email do |n|
+    "user#{n}@test.com"
   end
   factory :user do
     name
     number {'123456'} 
+    email
+    password {'123123'}
+    password_confirmation {'123123'} 
   end
 end
